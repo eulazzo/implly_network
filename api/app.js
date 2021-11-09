5748046941f8dc469a5ff598548a522bf4c95a3d
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-09 11:27:16 -0300

    pulling out the useCreateIndex: true from mongoose connectionbecause of bugs with the save method

--- api/app.js
+++ api/app.js
@@ -1,36 +1,33 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const dotenv = require('dotenv')
-const helmet = require('helmet')
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+ 
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
- },  
- () => {
-    console.log('connected to mongoDb');
-  } 
-)
+mongoose
+  .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true}
+  )
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
 
 //MIDDLEWARES
 app.use(express.json())
 app.use(helmet())
-app.use(morgan())
-app.use('api/users',userRoute)
-app.use('api/auth',authRoute)
+app.use(morgan('common'))
 
-app.get('/',(req,res)=>res.send('welcome Home Page'))
+app.use('/api/users',userRoute)
+app.use('/api/auth',authRoute)
+ 
 
-app.listen(8080,() => {
-  console.log('backend server is running!')
-})
+app.listen(8080,()=> console.log('backend server is running!'))
 
 
 
@@ -56,9 +53,4 @@ app.listen(8080,() => {
 
 
 
-// const userRoute = require('./routes/users')
-// const authRoute = require('./routes/auth')
-// const postRoute = require('./routes/posts')
-// const messageRoute = require('./routes/messages')
-// const conversationRoute = require('./routes/conversations')
-// const path = require("path")
+ 
