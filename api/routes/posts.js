81d33b234010e7fb457abace7fddadfabd0d48f9
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-09 16:04:13 -0300

    likes, dislike, get post and others routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -53,5 +53,62 @@ router.delete('/:id',async(req,res)=> {
   }
 })
 
+ //like or dislike a post
+router.put('/:id/like', async(req,res) => {
+  
+  try{
+    const post = await Post.findById(req.params.id)
+
+    if(!post.likes.includes(req.body.userId)){
+      await post.updateOne({$push:{likes:req.body.userId}})
+      res.status(200).json('The post has been liked!')
+    }else{
+      await post.updateOne({$pull:{likes:req.body.userId}})
+      res.status(200).json('The post has been disliked!')
+    }
+
+  }catch(err){
+    res.status(403).json(err)
+  }
+
+})
+   
+//get a post
+router.get('/:id', async(req,res)=> {
+  try {
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+//get timeline posts
+router.get('/timeline/:userId',async(req,res)=>{
+  try{
+    const currentUser = await User.findById(req.params.userId);
+    const userPosts = await Post.find({userId:currentUser._id})
+    const friendPosts = await Promise.all(
+      currentUser.followings.map(followUserId=>Post.find({userId:followUserId}))
+      )
+      res.status(200).json(userPosts.concat(...friendPosts))
+    }catch(err) {
+    res.status(500).json(err)
+  }
  
+})
+
+//get all posts of one user
+router.get('/profile/:username',async(req,res)=>{
+  try{
+     const user = await User.findOne({username:req.params.username})
+     const posts =await Post.find({userId:user._id})
+     res.status(200).json(posts)
+    }catch(err) {
+    res.status(500).json(err)
+  }
+ 
+})
+
 module.exports = router
