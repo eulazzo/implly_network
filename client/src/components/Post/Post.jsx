60589308b1e8a93acbebe679defd80611596ae42
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-10 20:51:58 -0300

    like funcionality with useState hook

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,8 +1,17 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export const Post = ({ post }) => {
+  const [like, setLike] = useState(post.like);
+  const [isliked, setIsLiked] = useState(false);
+
+  const likeHandler = ()=> {
+    setLike(isliked ? like-1 : like+1)
+    setIsLiked(!isliked)
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -28,9 +37,19 @@ export const Post = ({ post }) => {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} likes</span>
+            <img
+              className="likeIcon"
+              src="assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              onClick={likeHandler}
+              src="assets/heart.png"
+              alt=""
+            />
+            <span className="postLikeCounter">{like} likes</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
